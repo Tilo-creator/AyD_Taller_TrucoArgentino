f4d41d966b6e87808c7f7232ff8d7ac3c6c54550
Francisco Pedernera <pancho25pedernera@gmail.com>
2024-05-15 16:39:49 -0300

    Se modigico el modelo de usuario

--- models/user.rb
+++ models/user.rb
@@ -1,2 +1,8 @@
+# models/user.rb
 class User < ActiveRecord::Base
-end
+    validates :names, presence: true
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
+  end
+  
