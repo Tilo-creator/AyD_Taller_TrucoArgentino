9547f288c26fe5d4e8ca448b5892a70ae27495bf
Francisco Pedernera <pancho25pedernera@gmail.com>
2024-09-11 12:54:25 -0300

    Se terminaron unos test y se modco la parte forntend

--- app.rb
+++ app.rb
@@ -1,4 +1,3 @@
-# app.rb
 require 'sinatra'
 require 'sinatra/activerecord'
 require './models/user'
@@ -9,73 +8,54 @@ require './models/application_record'
 
 set :database_file, './config/database.yml'
 
-set :database_file, './config/database.yml'
-
 class App < Sinatra::Application
   enable :sessions
 
+  # Página de inicio que maneja tanto el inicio de sesión como el registro
   get '/' do 
     erb :inicio
   end 
 
-  get '/login' do
-    erb :login
-  end
-
-  post '/login' do
-    @user = User.find_by(username: params[:username], password: params[:password])
-    
-    if @user
-      session[:user_id] = @user.id
-      # Redirigir al usuario según el juego seleccionado
-      case params[:juego]
-      when 'truco'
-        redirect '/juegos/truco'
-      when 'poker'
-        redirect '/juegos/poker'
-      when 'escoba'
-        redirect '/juegos/escoba'
-      else
+  post '/' do
+    if params[:action] == 'login'
+      # Manejo del inicio de sesión
+      @user = User.find_by(username: params[:username], password: params[:password])
+      
+      if @user
+        session[:user_id] = @user.id
         redirect '/juegos'
+      else
+        erb :inicio, locals: { mensaje: 'Credenciales incorrectas. Inténtalo de nuevo.' }
+      end
+    elsif params[:action] == 'register'
+      
+      
+      # Manejo del registro
+      if params[:password] == params[:confirm_password]
+        @user = User.new(names: params[:fullname], username: params[:username], email: params[:email], password: params[:password])
+        redirect '/juegos'
+        if @user.save
+          session[:user_id] = @user.id
+          @statistic = Statistic.new(cantidadDePreguntaRespondidas: "0", cantPregRespondidasBien: "0", CantPregRespondidasMal:"0", user_id: @user.id)
+          @statistic.save
+          redirect '/juegos'
+        else
+          erb :inicio, locals: { mensaje: 'Hubo un error al registrar el usuario. Inténtalo de nuevo.' }
+        end
+      else
+        erb :inicio, locals: { mensaje: 'Las contraseñas no coinciden.' }
       end
     else
-      'Credenciales incorrectas. Inténtalo de nuevo.' 
+      redirect '/'
     end
   end
-  
+
   get '/perfil' do
     if session[:user_id]
       @user = User.find(session[:user_id])
       erb :perfil, locals: { user: @user }
     else
-      redirect '/login'
-    end
-  end
-
-  get '/entre' do
-    "Credenciales válidas"
-  end
-
-  get '/lecciones' do
-    @lecciones = Lesson.all
-    erb :lecciones
-  end
-
-  get '/registro' do
-    erb :registro
-  end
-
-  post '/registro' do
-    
-    @user = User.new(names: params[:names], username: params[:username], email: params[:email], password: params[:password])
-  
-    if @user.save
-      session[:user_id] = @user.id
-      @statistic = Statistic.new(cantidadDePreguntaRespondidas: "0", cantPregRespondidasBien: "0", CantPregRespondidasMal:"0", user_id: @user.id)
-      @statistic.save
-      redirect '/juegos'
-    else
-      erb :registro, locals: { mensaje: 'Hubo un error al registrar el usuario. Inténtalo de nuevo.' }
+      redirect '/'
     end
   end
 
@@ -84,15 +64,12 @@ class App < Sinatra::Application
     erb :juegos, locals: { juegos: juegos }
   end
 
-
-
-
   get '/juegos/poker' do
-    erb :poker # Ruta para cargar la vista poker.erb
+    erb :poker
   end
 
   get '/juegos/escoba' do
-    erb :escoba # Ruta para cargar la vista .erb
+    erb :escoba
   end
 
   get '/juegos/truco' do
@@ -112,10 +89,9 @@ class App < Sinatra::Application
       @statistic = @user.statistics.last
       erb :estadisticas, locals: { estadistic: @statistic }
     else
-      redirect '/login'
+      redirect '/'
     end
   end
-
 
   post '/preguntas/responder' do
     if session[:user_id]
@@ -131,26 +107,18 @@ class App < Sinatra::Application
         @statistic.cantidadDePreguntaRespondidas += 1
         @statistic.cantPregRespondidasBien += 1
         session[:resultado] = "¡Respuesta correcta!"
-
       else
         @statistic.cantidadDePreguntaRespondidas += 1
         @statistic.CantPregRespondidasMal += 1
         session[:resultado] = "Respuesta incorrecta. La respuesta correcta es: #{@question.correct_answer}"
       end
       @statistic.save
-      session[:mostrar_mensaje] = true # Indica que se debe mostrar el mensaje
+      session[:mostrar_mensaje] = true
       redirect '/preguntas'
     else
-      redirect '/login'
+      redirect '/'
     end
   end
-
-
-
 end
-
-
-
-
 
 App.run! if __FILE__ == $0
