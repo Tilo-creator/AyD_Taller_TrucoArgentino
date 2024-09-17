01347d2fe8c364629632dc18264a47b53e7f61e2
Ricardo Trimboli <riki.trimboli@gmail.com>
2024-09-17 12:28:46 -0300

    [fixes

--- db/migrate/20240917142434_crate_table_vidas.rb
+++ db/migrate/20240917142434_crate_table_vidas.rb
@@ -4,6 +4,7 @@ class CrateTableVidas < ActiveRecord::Migration[7.1]
       t.integer :cantidadDeVidas, default: 3
       t.references :user, foreign_key:true
 
+      
       t.datetime :created_at
       t.datetime :updated_at
     end
