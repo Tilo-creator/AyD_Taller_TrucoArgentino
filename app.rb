eec581bbfc6f342a7e3e435de9d0a0ee1d19102b
Francisco Pedernera <pancho25pedernera@gmail.com>
2024-11-02 19:33:53 -0300

    cambios app.rb

--- app.rb
+++ app.rb
@@ -9,11 +9,26 @@ require './models/statistic'
 require './models/life'
 require './models/application_record'
 require './models/level'
+require_relative './controllers/estadisticas_controller'
+require_relative './controllers/game_controller'
+require_relative './controllers/question_controller'  # Asegúrate de que este sea correcto
+require_relative './controllers/user_controller'
+
+use EstadisticasController
+use GameController
+use LessonController
+use Questioncontroller
+use UserController
 
 set :database_file, './config/database.yml'
+set :public_folder, 'public'
 # Clase encargada de manejar la lógica principal de la aplicación
 class App < Sinatra::Application
   enable :sessions
+
+  configure do
+    set :views, './views'
+  end
 
   get '/' do
     erb :inicio
@@ -44,52 +59,16 @@ class App < Sinatra::Application
     erb :estadisticasPreguntas
   end
 
-  # Ruta para el formulario de registro
-  get '/login' do
-    erb :login # Vista para el formulario de registro
-  end
 
-  # Ruta para manejar la creación de usuarios
-  post '/login' do
-    if params[:password] == params[:confirm_password]
-      @user = User.new(
-        fullname: params[:fullname],
-        username: params[:username],
-        email: params[:email],
-        password: params[:password]
-      )
-      if @user.save
-        # Crear un nivel inicial para el usuario con level_number en 0
-        @user.create_level(level_number: 0)
 
-        session[:user_id] = @user.id
-        redirect '/juegos'
-      else
-        erb :login, locals: { mensaje: 'Hubo un error al crear tu cuenta. Inténtalo de nuevo.' }
-      end
-    else
-      erb :login, locals: { mensaje: 'Las contraseñas no coinciden. Inténtalo de nuevo.' }
-    end
-  end
 
-  get '/perfil' do
-    if session[:user_id]
-      @user = User.find(session[:user_id])
-      erb :perfil, locals: { user: @user }
-    else
-      redirect '/'
-    end
-  end
 
   get '/lecciones' do
     @lecciones = Lesson.all
     erb :lecciones
   end
 
-  get '/juegos' do
-    juegos = %w[Truco Poker Escoba]
-    erb :juegos, locals: { juegos: juegos }
-  end
+
 
   get '/juegos/poker' do
     erb :poker
@@ -99,14 +78,6 @@ class App < Sinatra::Application
     erb :escoba
   end
 
-  get '/juegos/truco' do
-    @user = User.find(session[:user_id])
-    redirect '/trucoAdming' if @user.isAdmin
-    @lessons = Lesson.all
-    @life = @user.lives.last
-    erb :truco, locals: { lessons: @lessons, vida: @life }
-  end
-
   get '/trucoAdming' do
     @user = User.find(session[:user_id])
     @lessons = Lesson.all
@@ -114,80 +85,7 @@ class App < Sinatra::Application
     erb :trucoAdming, locals: { lessons: @lessons, vida: @life }
   end
 
-  get '/generarPregunta' do
-    erb :formulario
-  end
 
-  post '/generarPregunta' do
-    @question = Question.new(description: params[:description], options: params[:options],
-                             correct_answer: params[:correct_option])
-    @question.save
-    redirect '/trucoAdming'
-  end
-
-  get '/lecciones' do
-    @lecciones = Lesson.all
-    erb :lecciones
-  end
-
-  get '/preguntas' do
-    @user = User.find(session[:user_id])
-    @life = @user.lives.last
-    @question = Question.order('RANDOM()').first
-    @resultado = session.delete(:resultado)
-    erb :preguntas, locals: { vida: @life }
-  end
-
-  get '/estadisticas' do
-    if session[:user_id]
-      @user = User.find(session[:user_id])
-      @statistic = @user.statistics.last
-      erb :estadisticas, locals: { estadistic: @statistic }
-    else
-      redirect '/'
-    end
-  end
-
-  post '/preguntas/responder' do
-    if session[:user_id]
-      @user = User.find(session[:user_id])
-      @statistic = @user.statistics.last
-      @life = @user.lives.last
-
-      if @life.cantidadDeVidas.positive?
-        @question = Question.find(params[:pregunta_id])
-
-        @statistic.cantidadDePreguntaRespondidas += 1
-        if @question.correct_answer?(params[:respuesta])
-          @question.vecesRespondidasBien += 1
-          @statistic.cantPregRespondidasBien += 1
-          @statistic.total_points += 5
-          session[:resultado] = '¡Respuesta correcta!'
-        else
-          @statistic.cantPregRespondidasMal += 1
-          @question.vecesRespondidasMal += 1
-          @statistic.total_points -= 3
-          @life.cantidadDeVidas -= 1
-          session[:resultado] = "Respuesta incorrecta. La respuesta correcta es: #{@question.correct_answer}"
-        end
-
-        # Guardar cambios en la estadística y vidas
-        @statistic.save
-        @life.save
-        @question.save
-
-        # Mostrar mensaje y redirigir a la página de preguntas
-        session[:mostrar_mensaje] = true
-      else
-        # Si no hay más vidas, mostrar el mensaje correspondiente
-        session[:resultado] = 'No tienes más vidas'
-      end
-      redirect '/preguntas'
-    else
-      # Si no hay sesión activa, redirigir a la página principal
-      redirect '/'
-    end
-  end
 
   def calcular_nivel(puntos)
     case puntos
