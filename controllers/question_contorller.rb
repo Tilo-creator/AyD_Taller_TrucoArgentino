5d6d5f4b3b28643c45cd161d7b4805b2a3c42e09
Francisco Pedernera <pancho25pedernera@gmail.com>
2024-11-04 18:54:22 -0300

    Se hizieron cambion en el controller de question y se modifico el fron para mostrar el nivel del usuairo

--- controllers/question_contorller.rb
+++ controllers/question_contorller.rb
@@ -5,71 +5,90 @@ require './models/statistic'
 require './models/life'
 require './models/application_record'
 require './models/level'
+
 class QuestionController < Sinatra::Base
-    
-    configure do
-        set :views, './views'
-    end
-    
-    get '/generarPregunta' do
-      erb :formulario
-    end
+  configure do
+    set :views, './views'
+  end
   
-    post '/generarPregunta' do
-      @question = Question.new(description: params[:description], options: params[:options], correct_answer: params[:correct_option])
-      @question.save
-      redirect '/trucoAdming'
-    end
+  # Rutas
+  get '/generarPregunta' do
+    erb :formulario
+  end
+
+  post '/generarPregunta' do
+    @question = Question.new(description: params[:description], options: params[:options], correct_answer: params[:correct_option])
+    @question.save
+    redirect '/trucoAdmin'
+  end
+
+  get '/preguntas' do
+    @user = User.find(session[:user_id])
+    @life = @user.lives.last
+    @question = Question.order("RANDOM()").first
+    @resultado = session.delete(:resultado)
+    erb :preguntas, locals: { vida: @life }
+  end
   
-    get '/preguntas' do
+  post '/preguntas/responder' do
+    if session[:user_id]
       @user = User.find(session[:user_id])
+      @statistic = @user.statistics.last
       @life = @user.lives.last
-      @question = Question.order("RANDOM()").first
-      @resultado = session.delete(:resultado)
-      erb :preguntas, locals: { vida: @life }
-    end
-  
-    post '/preguntas/responder' do
-      if session[:user_id]
-        @user = User.find(session[:user_id])
-        @statistic = @user.statistics.last 
-        @life = @user.lives.last
-        
-        if @life.cantidadDeVidas > 0
-          @question = Question.find(params[:pregunta_id])
-    
-          if @question.correct_answer?(params[:respuesta])
-            @statistic.cantidadDePreguntaRespondidas += 1
-            @question.vecesRespondidasBien += 1 
-            @statistic.cantPregRespondidasBien += 1
-            @statistic.total_points += 5
-            session[:resultado] = "¡Respuesta correcta!"
-          else
-            @statistic.cantidadDePreguntaRespondidas += 1
-            @statistic.cantPregRespondidasMal += 1
-            @question.vecesRespondidasMal += 1
-            @statistic.total_points -= 3
-            @life.cantidadDeVidas -= 1
-            session[:resultado] = "Respuesta incorrecta. La respuesta correcta es: #{@question.correct_answer}"
-          end
-    
-          # Guardar cambios en la estadística y vidas
-          @statistic.save
-          @life.save
-          @question.save
-    
-          # Mostrar mensaje y redirigir a la página de preguntas
-          session[:mostrar_mensaje] = true
-          redirect '/preguntas'
-        else
-          # Si no hay más vidas, mostrar el mensaje correspondiente
-          session[:resultado] = "No tienes más vidas"
-          redirect '/preguntas'
-        end
+
+      if @life.cantidadDeVidas > 0
+        procesar_respuesta(params[:pregunta_id], params[:respuesta])
+        actualizar_nivel_usuario(@statistic.total_points)
+        guardar_cambios
+        session[:mostrar_mensaje] = true
+        redirect '/preguntas'
       else
-        # Si no hay sesión activa, redirigir a la página principal
-        redirect '/'
+        session[:resultado] = "No tienes más vidas"
+        redirect '/preguntas'
       end
+    else
+      redirect '/'
     end
   end
   
+  # Métodos auxiliares
+
+  def procesar_respuesta(pregunta_id, respuesta)
+    @question = Question.find(pregunta_id)
+    if @question.correct_answer?(respuesta)
+      actualizar_estadisticas_correcta
+      session[:resultado] = "¡Respuesta correcta!"
+    else
+      actualizar_estadisticas_incorrecta
+      session[:resultado] = "Respuesta incorrecta. La respuesta correcta es: #{@question.correct_answer}"
+    end
+  end
+  
+  def actualizar_estadisticas_correcta
+    @statistic.cantidadDePreguntaRespondidas += 1
+    @question.vecesRespondidasBien += 1 
+    @statistic.cantPregRespondidasBien += 1
+    @statistic.total_points += 5
+  end
+  
+  def actualizar_estadisticas_incorrecta
+    @statistic.cantidadDePreguntaRespondidas += 1
+    @statistic.cantPregRespondidasMal += 1
+    @question.vecesRespondidasMal += 1
+    @statistic.total_points -= 3
+    @life.cantidadDeVidas -= 1
+  end
+  
+  def actualizar_nivel_usuario(puntos)
+    nuevo_nivel = calcular_nivel(puntos)
+    if @user.level.level_number != nuevo_nivel
+      @user.level.update(level_number: nuevo_nivel)
+    end
+  end
+
+  def guardar_cambios
+    @statistic.save
+    @life.save
+    @question.save
+  end
+end
